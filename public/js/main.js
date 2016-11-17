62dc7add140b081f6bc698a928dd46a0677110b6
Noah Wimmer <nwimmer123@yahoo.com>
2016-11-17 14:45:17 -0800

    added more languages

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,20 @@
 $(document).ready(function(){
-  console.log("Titties!!");
+  console.log("Hi Mother Fucker");
+
+  getTxt = function (){
+    $.ajax({
+      url: 'languages/english.txt',
+      success: function (data){
+        data = data.split(/\r?\n/)
+        console.log(data)
+      }
+    })
+  };
+
+  $("#languageSelect").on("click", function() {
+    getTxt();
+  });
 
 });
+
 
--- server.js
+++ server.js
@@ -2,19 +2,6 @@ var express = require('express');
 var app = express();
 var html = require('html');
 
-// var fs = require('fs');
-// var readline = require('readline');
-
-// var filename = process.argv[2];
-// readline.createInterface({
-//     input: fs.createReadStream(filename),
-//     terminal: false
-// }).on('line', function(line) {
-//    console.log('Line: ' + line);
-// });
-
-// var array = load(filename);
-
 // serve static files from public folder
 app.use('/',express.static(__dirname + '/public'));
 
@@ -25,6 +12,17 @@ app.use('/',express.static(__dirname + '/public'));
 app.get('/', function homepage (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
+
+getTxt = function (){
+
+  $.ajax({
+    url: 'languages/english.txt',
+    success: function (data){
+      console.log("CLICKED");
+      console.log(data)
+    }
+  })
+}
 
 /**********
  * SERVER *
