e8b33eed02722612b3e78a2826985ff2aa9bda6d
Noah Wimmer <nwimmer123@yahoo.com>
2016-11-17 16:18:51 -0800

    array attributes stored as object

--- public/js/main.js
+++ public/js/main.js
@@ -3,25 +3,41 @@ $(document).ready(function(){
 
   $("#generate").on("click", function() {
     getAttributes();
-    //getTxt();
+    getTxt();
   });
 
 });
 
-getTxt = function (){
+function getTxt(){
   var language = $("#language").val();
   $.ajax({
     url: "languages/" + language + ".txt",
     success: function (data){
-      data = data.split(/\r?\n/)
-      console.log(data)
+      data = data.split(/\r?\n/);
+      generateArray(data);
     }
   })
 };
 
+// function getAttributes() {
+//   var numWords = $("#numWords").val();
+//   var minLetters = $("#minNum").val();
+//   var maxLetters = $("#maxNum").val();
+// };
+
+function generateArray(data){
+  console.log(data);
+  console.log(arrayAttributes);
+};
+
+//store array attributes
+ var arrayAttributes = [];
 function getAttributes() {
-  var numWords = $("#numWords").val();
-  var minLetters = $("#minNum").val();
-  var maxLetters = $("#maxNum").val();
+ 
+  arrayAttributes.push({
+    numWords: parseInt($("#numWords").val()),
+    minLetters: parseInt($("#minNum").val()),
+    maxLetters: parseInt($("#maxNum").val()),
+  })
 }
 
