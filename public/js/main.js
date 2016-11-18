9a032d982e7bab4c6def1b108076e055389b4ea5
Noah Wimmer <nwimmer123@yahoo.com>
2016-11-17 20:43:03 -0800

    adeed comments and removed obsolete code

--- public/js/main.js
+++ public/js/main.js
@@ -4,10 +4,12 @@ $(document).ready(function(){
   $("#generate").on("click", function() {
     getAttributes();
     getTxt();
+    //generateWords();
   });
 
 });
 
+//gets the root word list
 function getTxt(){
   var language = $("#language").val();
   $.ajax({
@@ -19,27 +21,20 @@ function getTxt(){
   })
 };
 
+//creating the new array
 function generateArray(data){
-  console.log(arrayAttributes[0].minLetters);
   var finalArray = data.filter(filterByLength);
+  finalArray = shuffle(finalArray);
   console.log(finalArray);
 };
 
+//filters array by desired word length
 function filterByLength(obj) {
   if ((obj.length >= arrayAttributes[0].minLetters) && (obj.length <= arrayAttributes[0].maxLetters)) {
     return true
   } else {
     return false
   }
-};
-
-function removeWords(){
-  if (this.length < arrayAttributes[0].minLetters) {
-    this.slice
-  };
-  if (this.length > arrayAttributes[0].maxLetters) {
-    this.slice
-  };
 };
 
 //store array attributes
@@ -51,4 +46,29 @@ function getAttributes() {
     maxLetters: parseInt($("#maxNum").val()),
   })
 };
+
+//randomize word array order
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex ;
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+  return array;
+}
+
+//populate array w different words for each game
+var gameWords = [];
+function generateWords() {
+  shuffle(array);
+  for(var i = 0; i < arrayAttributes[0].numWords; i ++) {
+    gameWords.push(words[i]);
+   }
+}
 
